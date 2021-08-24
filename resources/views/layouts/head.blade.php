43af43a1908528271ea68fffa1fd20ad58f6cdc4
dinarabdulholik_ <dinartrevor@gmail.com>
2021-08-24 11:18:35 +0700

    Typo

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,7 +1,7 @@
 <head>
 	{{--  Required Meta Tags --}}
 	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, shrink-to-fit=no" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no" />
 	<meta http-equiv="cache-control" content="max-age=0" />
 	<meta http-equiv="cache-control" content="no-cache" />
 	<meta http-equiv="expires" content="0" />
